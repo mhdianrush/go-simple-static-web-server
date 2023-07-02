0a4e837fc51e74029a874e6a1c7cfdf8d3806087
mhd ian rush <mhdianrush@gmail.com>
2023-07-02 07:44:28 +0700

    Update main.go

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ func helloHandler(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 	if request.Method != "GET" {
-		// for example we user GET http method
 		http.Error(writer, "Method Is Not Supported", http.StatusNotFound)
 		return
 	}
@@ -22,7 +21,6 @@ func helloHandler(writer http.ResponseWriter, request *http.Request) {
 
 func formHandler(writer http.ResponseWriter, request *http.Request) {
 	err := request.ParseForm()
-	// using ParseForm because user in the client side will send a data to a form
 	if err != nil {
 		fmt.Fprintf(writer, "ParseForm() err is%v", err)
 		return
@@ -30,7 +28,7 @@ func formHandler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprint(writer, "POST request is SuccessFul\n")
 	name := request.FormValue("name")
 	address := request.FormValue("address")
-	// name and address variable adove is from the form.html file inside the static folder format for html
+	
 	fmt.Fprintf(writer, "Name is %s\n", name)
 	fmt.Fprintf(writer, "Address is %s\n", address)
 }
